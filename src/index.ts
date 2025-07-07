d626122272814777a5e8f8a45cffef6c20005bd5
Péter Szedmák <thazulk@gmail.com>
2025-07-07 08:23:11 +0200

    Update package.json with project metadata and enhance readiness check in index.ts; introduce readiness handler in new readiness.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
 import express from "express";
-import path from "path";
+
+import { handlerReadiness } from "./api/readiness.js";
 
 const app = express();
 const PORT = 8080;
 
-const handleReadiness = (req: express.Request, res: express.Response) => {
-  res.set({
-    "Content-Type": "text/plain",
-    charset: "utf-8",
-  });
-  res.send("OK");
-};
 app.use("/app", express.static("./src/app"));
-app.get("/healthz", handleReadiness);
+
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
