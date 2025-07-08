dba94a72f97c128a3a15a0e9afd9f3b7421d1703
Péter Szedmák <thazulk@gmail.com>
2025-07-08 09:45:09 +0200

    Add chirp validation endpoint and JSON response utilities; implement handler for validating chirp length in chirps.ts and update index.ts to include new route.

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import {
   middlewareMetricsInc,
 } from "./api/middleware.js";
 
+import { handlerChirpsValidate } from "./api/chirps.js";
+
 const app = express();
 const PORT = 8080;
 
@@ -18,6 +20,8 @@ app.use("/app", express.static("./src/app"));
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerMetrics);
 app.post("/admin/reset", handlerReset);
+
+app.post("/api/validate_chirp", handlerChirpsValidate);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
