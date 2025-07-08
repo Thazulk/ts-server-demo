6a5e01af5e216ae449e89f23d9e3c3357e050a76
Péter Szedmák <thazulk@gmail.com>
2025-07-08 10:43:28 +0200

    Implement profanity filtering in chirp validation; replace original chirp body with cleaned version in response to enhance content moderation.

--- src/api/chirps.ts
+++ src/api/chirps.ts
@@ -10,12 +10,24 @@ export async function handlerChirpsValidate(req: Request, res: Response) {
   const params: parameters = req.body;
 
   const maxChirpLength = 140;
+
+  const profaneWords = ["kerfuffle", "sharbert", "fornax"];
+  const cleanedBody = params.body
+    .split(" ")
+    .map((word) => {
+      if (profaneWords.includes(word.toLowerCase())) {
+        return "****";
+      }
+      return word;
+    })
+    .join(" ");
+
   if (params.body.length > maxChirpLength) {
     respondWithError(res, 400, "Chirp is too long");
     return;
   }
 
   respondWithJSON(res, 200, {
-    valid: true,
+    cleanedBody,
   });
 }
